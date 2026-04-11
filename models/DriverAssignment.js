2dfc511c5efaa32d38e2ffacd484e49da4dd0e50
Nziza Oscar <santosjuniorsoftwareengineer1@gmail.com>
2026-04-11 16:06:01 +0200

    feat: models/DriverAssignment.js

--- models/DriverAssignment.js
+++ models/DriverAssignment.js
@@ -1,3 +1,4 @@
+// models/DriverAssignment.js
 const { DataTypes } = require("sequelize");
 const sequelize = require("../config/database");
 
@@ -13,21 +14,37 @@ const DriverAssignment = sequelize.define(
     bus_id: {
       type: DataTypes.UUID,
       allowNull: false,
+      references: {
+        model: 'buses',
+        key: 'id'
+      }
     },
 
     driver_id: {
       type: DataTypes.UUID,
       allowNull: false,
+      references: {
+        model: 'users',  // References User (with role='driver')
+        key: 'id'
+      }
     },
 
     company_id: {
       type: DataTypes.UUID,
       allowNull: false,
+      references: {
+        model: 'companies',
+        key: 'id'
+      }
     },
 
     assigned_by: {
       type: DataTypes.UUID,
       allowNull: false,
+      references: {
+        model: 'users',  // References User (company_admin or admin)
+        key: 'id'
+      }
     },
 
     assigned_at: {
@@ -37,17 +54,31 @@ const DriverAssignment = sequelize.define(
 
     unassigned_at: {
       type: DataTypes.DATE,
+      allowNull: true,
     },
 
-    reason: DataTypes.TEXT,
-    notes: DataTypes.TEXT,
+    reason: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+
+    notes: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
   },
   {
     tableName: "driver_assignments",
     timestamps: true,
     underscored: true,
+    indexes: [
+      { fields: ['bus_id'] },
+      { fields: ['driver_id'] },
+      { fields: ['company_id'] },
+      { fields: ['assigned_by'] },
+      { fields: ['assigned_at'] }
+    ]
   }
 );
-
 
 module.exports = DriverAssignment;
